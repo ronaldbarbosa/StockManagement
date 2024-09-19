36048b9785d2060cb48cd68f1692f03e5ba3c3e5
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2024-09-18 22:41:43 -0300

    feat(web): style improvements in login and register pages; created sidebar component.

---
+++ StockManagement.BlazorWebApp/Components/Sidebar.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StockManagement.BlazorWebApp.Components
+{
+    public partial class Sidebar : ComponentBase
+    {
+        #region services
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region overrides
+        #endregion
+
+        #region methods
+        #endregion
+    }
+}
